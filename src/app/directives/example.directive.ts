dadeb1b0965832193d43b3028114e51868a6b5d5
furkanbircan <furkan.bircan@roofstacks.com>
2024-04-01 13:37:52 +0300

    update example component and custom directives

--- src/app/directives/example.directive.ts
+++ src/app/directives/example.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
-declare var $: any;
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
+declare var $: any; //Declare the $ variable to use Jquery
 @Directive({
-  selector: '[appExample]'
+  selector: '[appExample]',
   //selector: '.app-example' class directive
 })
 export class ExampleDirective implements OnInit {
   // ElementRef allows us to access a local element within a View
-  @Input() appExample: string = ""; //can use the input decarator to get a parameter to the directive
+  @Input() appExample: string = ''; //can use the input decorator to get a parameter to the directive
 
-  @HostBinding('style.color') //HostBinding allows us to bind a property of the element that uses the directive
-  textColor : string = "white";
-  constructor(private elementRef : ElementRef) {
-
-  }
   ngOnInit(): void {
     this.elementRef.nativeElement.style.backgroundColor = this.appExample; //Change the style of an html element with ElementRef
     this.elementRef.nativeElement.style.cursor = 'pointer';
@@ -20,6 +22,9 @@ export class ExampleDirective implements OnInit {
   }
   @HostListener('click') //HostListener continuously listens for the html tag that uses the (appExample) directive
   onClick() {
-    alert("Hello World");
+    alert('Hello World');
   }
+  @HostBinding('style.color') //HostBinding allows us to bind a property of the element that uses the directive
+  textColor: string = 'white';
+  constructor(private elementRef: ElementRef) {}
 }
