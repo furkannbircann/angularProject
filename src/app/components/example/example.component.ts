f1cd13173fbc3392683dd1247b15fd05825aa1c9
furkanbircan <furkan.bircan@roofstacks.com>
2024-10-25 14:46:11 +0300

    Refactor

--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.scss']
 })
-export class ExampleComponent {
+export class ExampleComponent implements OnChanges {
+
+  @Input() title: string = 'Default Title';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Life Cycle 1.step -> ngOnChanges', changes); //@Input() value change detection
+  }
   names: string[] = ['John', 'Doe', 'Jane', 'Doe'];
   name: string = 'John Doe';
   date : Date = new Date();
+
+
 }
--- src/app/pipes/example.pipe.ts
+++ src/app/pipes/example.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ExamplePipe implements PipeTransform {
 
-  transform(value: string,a: number,b:number): string {
+  transform(value: string,a?: number,b?:number): string {
     return value.slice(a,b); // return value.slice(0,5) => 'John '
   }
 }
